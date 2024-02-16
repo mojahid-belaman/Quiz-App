41c1130cc55e2f6cd6affe740443d3dcff935a8a
mojahid-belaman <mojahidbelaman@gmail.com>
2024-02-16 10:40:32 +0100

    create: final result of quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 82, 39, 176),
-                Color.fromARGB(255, 105, 39, 176)
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz(),
   );
 }
