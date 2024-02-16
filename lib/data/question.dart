a14765aa08fa1df0856e1f9d58f2c3cb91a43d5c
mojahid-belaman <mojahidbelaman@gmail.com>
2024-02-16 10:37:31 +0100

    create: dummy data about question and answers

---
+++ lib/data/question.dart
@@ -0,0 +1,20 @@
+import 'package:quiz_app/modal/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the Current Foundr In ORA?',
+    ['Hakim', 'Anass', 'Daifi', 'Ophen'],
+  ),
+  QuizQuestion(
+    'Currently,How many People leaved ORA?',
+    ['3', '7', '5', '2'],
+  ),
+  QuizQuestion(
+    'What Is The New Stack Mobile in ORA?',
+    ['Flutter', 'Xamarin', 'Swift', 'React Native'],
+  ),
+  QuizQuestion(
+    'What are The Best Founder In ORA?',
+    ['Hakim', 'Ophen', 'houmame', 'Lemock'],
+  ),
+];
